9d86671df2f77b8732cf49ce33f89242cd97b6ca
alfiejeong <161536788+alfiejeong@users.noreply.github.com>
2026-04-12 21:05:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var isDataLoaded = false;
 var boardData = [];
 
 // [주의] 이 변수가 파일 내에 딱 하나만 있는지 반드시 확인하십시오.
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyJSg_7j4f17JzZroHDDq8s2YnzXXviNVzix-q52kEBBUg7p3ITYHFMqrQQVczCiQ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbytZVqg7VU49qfshjf1g2N3p9p1CgD9BYRhkscEr065-6aZ4mD2TbqSRgIVTaf47tjG/exec";
 
 // [보정] 수다방 데이터까지 포함한 통합 수급 로직
 // [수정] CORS 에러를 최소화하는 데이터 수급 로직
